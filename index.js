147aab2155d5e6c42f1bd4fefab91e15aee22a13
Felos <felmsilva2004@gmail.com>
2023-10-24 20:17:51 -0300

    Inserindo Dados no banco

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require("express")
 const express = require("express")
 const exphbs = require("express-handlebars")
 const mysql = require("mysql2")
@@ -5,7 +6,7 @@ const mysql = require("mysql2")
 const app = express()
 
 app.engine("handlebars", exphbs.engine())
-app.set("view.engine", "handlebars")
+app.set("view engine", "handlebars")
 
 app.use(express.static("public"))
 
@@ -14,6 +15,27 @@ app.use(express.urlencoded({
     extended:true
 }))
 app.use(express.json())
+
+app.post("/register/save", (req, res) => {
+    const {name, pageqty} = req.body
+   
+    const query =`
+    INSERT INTO books (name, pageqty)
+    VALUES ('${name}', '${pageqty}')
+    `
+    conn.query(query, (error) => {
+        if(error){
+            console.log(error)
+            return
+        }
+        res.redirect("/")
+    })
+    
+})
+
+app.get("/register", (req, res) => {
+    res.render("register")
+})
 
 app.get('/', (req,res) => {
     res.render("home")
